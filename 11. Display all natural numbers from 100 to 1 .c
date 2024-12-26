6407e5d2148ea3b278721788c18243abe8fb7f4a
musaib-h <musaibsh910@gmail.com>
2024-12-26 20:57:46 +0530

    Create 11. Display all natural numbers from 100 to 1 .c

---
+++ 11. Display all natural numbers from 100 to 1 .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Loop from 100 to 1
+    for (i = 100; i >= 1; i--) {
+        printf("%d ", i);
+    }
+
+    printf("\n"); // Move to the next line after printing numbers
+    return 0;
+}
