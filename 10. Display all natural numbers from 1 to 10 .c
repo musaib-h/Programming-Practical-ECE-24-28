177a412197fead5f7ee6f6a50c029a585109968d
musaib-h <musaibsh910@gmail.com>
2024-12-26 20:57:07 +0530

    Create 10. Display all natural numbers from 1 to 10 .c

---
+++ 10. Display all natural numbers from 1 to 10 .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Loop from 1 to 10
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n"); // Move to the next line after printing numbers
+    return 0;
+}
